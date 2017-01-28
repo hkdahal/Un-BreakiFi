ca9bb6e5b0607d8c545d4eb346b990f5da89b604
Hari Dahal <hxd1478@rit.edu>
2017-01-27 22:25:10 -0500

    Initial models setup

--- SystemApp/models.py
+++ SystemApp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Vendor(models.Model):
+    store_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.store_name
+
+
+class Transaction(models.Model):
+    auth_id = models.IntegerField()
+    date = models.DateField(default=timezone.now)
+    amount = models.FloatField()
+    location = models.CharField(max_length=10)
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def is_expense(self):
+        if self.amount < 0:
+            return True
+
+    def __str__(self):
+        return self.name

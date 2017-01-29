31a2fab29415854b19790d274aa0009f3f9f6b70
Hari Dahal <hxd1478@rit.edu>
2017-01-28 23:18:40 -0500

    View data per time

--- SystemApp/urls.py
+++ SystemApp/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^vendors/$', views.vendors),
-    url(r'^vendors/expense$', views.vendors),
+    url(r'^vendors/expense$', views.vendors_vs_expense),
+    url(r'^vendors/transactions', views.vendors_vs_transactions),
+    url(r'^dates', views.per_date),
+
+
 ]
--- SystemApp/views.py
+++ SystemApp/views.py
@@ -6,12 +6,11 @@ from .models import Vendor, Transaction
 
 import dateutil.parser as dtp
 
-import copy
-
 
 def index(request):
     transactions = Transaction.objects.all()
     total = [len(transactions), len(Vendor.objects.all())]
+
     return render(request, 'SystemApp/base.html',
                   context={
                       'transactions': transactions[:50],
@@ -19,36 +18,89 @@ def index(request):
                   })
 
 
-def vendors(request):
+def provide_vendors(per_expense=True, transactions=None):
     v = Vendor.objects.all().order_by('store_name')
-    total = len(v)
 
     v_expenses = []
 
     for ven in v:
         if ven.store_name != 'Paycheck':
-            v_expenses.append((ven, spent_money(ven)))
+            v_expenses.append((ven, spent_money(vendor=ven, per_expense=per_expense, transactions=transactions)))
 
-    sorted_by_expense = sorted(v_expenses, key=lambda x: -x[1][0])[:10]
-    sorted_by_transactions = sorted(v_expenses, key=lambda x: -x[1][1])[:10]
+    if per_expense:
+        data = sorted(v_expenses, key=lambda x: -x[1])[:10]
+    else:
+        data = sorted(v_expenses, key=lambda x: -x[1])[:10]
 
-    context = {'total': total, 'expense_vendors': sorted_by_expense,
-               'trans_vendors': sorted_by_transactions}
+    return data
+
+
+def vendors_vs_expense(request):
+    v = provide_vendors()
+    context = {'vendors': v, 'title': "Top 10: Vendors vs Expense"}
     return render(request, 'SystemApp/vendors.html', context)
 
 
-def spent_money(vendor):
-    transactions = Transaction.objects.filter(vendor=vendor)
+def vendors_vs_transactions(request):
+    v = provide_vendors(per_expense=False)
+    context = {'vendors': v, 'title': "Top 10: Vendors vs Transactions"}
+    return render(request, 'SystemApp/vendors.html', context)
+
+
+def spent_money(vendor=None, per_expense=True, transactions=None):
+    if not transactions:
+        if vendor:
+            transactions = Transaction.objects.filter(vendor=vendor)
+        else:
+            return 0
     total = 0
     for t in transactions:
         if t.is_expense():
             total += abs(t.amount)
-    return total, len(transactions)
+    if per_expense:
+        return total
+    else:
+        return len(transactions)
+
+
+def per_date(request):
+    start = dtp.parse('1/1/2013')
+    end = dtp.parse('1/10/2013')
+
+    if request.method == 'POST':
+        given = request.POST['subject']
+        given = given.split('-')
+        start = dtp.parse(given[0])
+        end = dtp.parse(given[1])
+        # print(start, end)
+
+    t = Transaction.objects.filter(date__gte=start, date__lt=end)
+    if t:
+        data = provide_vendors(transactions=t)
+    else:
+        data = []
+    context = {'vendors': data, 'title': "Top 10: Vendors vs Expense",
+               "start": str(start.date()), "end": str(end.date()),
+               "daily_info": per_days(t)
+               }
+    return render(request, 'SystemApp/dates.html', context)
+
+
+def per_days(transactions):
+    days = {}
+    days_expense_transactions = []
+    for t in transactions:
+        if t.date not in days:
+            days[t.date] = None
+            ts = transactions.filter(date=t.date)
+            days_expense_transactions.append(
+                (t.date, len(ts), spent_money(transactions=ts)))
+    return days_expense_transactions
 
 
 def parse():
     path = '/Users/hdahal/Desktop/Projects/Intuit/PlayGround/' \
-           'transaction-data/user-7.csv'
+           'transaction-data/user-0.csv'
     with open(path) as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
